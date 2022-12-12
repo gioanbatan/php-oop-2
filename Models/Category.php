aa900c47bc7f33cef4795ba09151106d46ff7702
gioanbatan <giovannibassani@hotmail.com>
2022-12-12 15:18:58 +0100

    first commit

---
+++ Models/Category.php
@@ -0,0 +1,20 @@
+<?php
+
+/** 
+ * Class Category of product
+ */
+class Category
+{
+    public $category;
+
+    /**
+     * Description
+     * @param {string} Category type
+     * @returns {any}
+     * 
+     */
+    function __contruct($category)
+    {
+        $this->category = $catregory;
+    }
+}
